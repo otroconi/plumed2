8b7f362f3b90f11b667755616e08db71bf8e7dd3
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-06-10 06:02:17 +0200

    Compile and install extension

--- src/maketools/find_modules.sh
+++ src/maketools/find_modules.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+extension=no
+
+if [ "$1" = --extension ] ; then
+  extension=yes
+fi
+
 cd ../
 for dir in *
 do
   
   if test -f "$dir/module.type"
   then
-    case "$(cat "$dir/module.type")" in
-    (always) echo $dir ;;
-    (default-on) test -f $dir.off || echo $dir ;;
-    (default-off) test -f $dir.on && echo $dir ;;
-    esac
+    if [ "$extension" = no ] ; then
+        case "$(cat "$dir/module.type")" in
+        (always) echo $dir ;;
+        (default-on) test -f $dir.off || echo $dir ;;
+        (default-off) test -f $dir.on && echo $dir ;;
+        esac
+    else
+        case "$(cat "$dir/module.type")" in
+        (default-on) test -f $dir.off || echo $dir ;;
+        (default-off) test -f $dir.on && echo $dir ;;
+        esac
+    fi
   fi
 done
