5487b772e196056657133debadab3323cbe2ee61
Giovanni Bussi <giovanni.bussi@gmail.com>
2020-12-15 07:55:43 +0100

    TEST
    two separate packages (ves and ddr here)

--- conda/plumed/build_plumed_contrib.sh
+++ conda/plumed/build_plumed_contrib.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+if [ "$#" = 1 ] ; then
+  extname="$1"
+  enabled="$1"
+else
+  extname=contrib
+  enabled=adjmat+annfunc+crystallization+dimred+drr+eds+fisst+funnel+logmfd+manyrestraints+maze+opes+pamm+piv+ves
+fi
+
+if [ "$1" = drr ] ; then
+  boost=yes
+  enable_boost=--enable-boost-serialization
+else
+  boost=no
+  enable_boost=
+fi
+
 if [[ $(uname) == "Linux" ]]; then
 # STATIC_LIBS is a PLUMED specific option and is required on Linux for the following reason:
 # When using env modules the dependent libraries can be found through the
@@ -17,7 +33,9 @@ export CXXFLAGS="${CXXFLAGS//-O2/-O3}"
 export LIBS="-lfftw3 -lgsl -lgslcblas -llapack -lblas -lxdrfile -lz $LIBS"
 
 # extra libraries
-export LIBS="-lboost_serialization $LIBS"
+if [ "$boost" = yes ] ; then
+  export LIBS="-lboost_serialization $LIBS"
+fi
 
 # python is disabled since it should be provided as a separate package
 # --disable-libsearch forces to link only explicitely requested libraries
@@ -25,8 +43,8 @@ export LIBS="-lboost_serialization $LIBS"
 # --disable-static-archive makes package smaller
 # --enable-asmjit enables bundled asmjit implementation
 ./configure --prefix=$PREFIX --disable-python --disable-libsearch --disable-static-patch --disable-static-archive --enable-asmjit \
-  --enable-boost-serialization --enable-modules=none+adjmat+annfunc+crystallization+dimred+drr+eds+fisst+funnel+logmfd+manyrestraints+maze+opes+pamm+piv+ves \
-  --enable-extension=contrib
+  $enable_boost --enable-modules=none+$enabled \
+  --enable-extension=$extname
 
 make -j${CPU_COUNT}
 make install
